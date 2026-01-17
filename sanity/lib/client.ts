6f7e22be20a97dd2915733da86eaea03884fff53
MominaAmir <mominaamir145@gmail.com>
2026-01-17 21:57:57 +0500

    Deployment issue fix 1

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,11 +1,15 @@
-// sanity/lib/client.ts
-import { createClient } from 'next-sanity'
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+import { createClient } from "@sanity/client";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+
+if (!projectId || !dataset) {
+  throw new Error("Missing Sanity environment variables");
+}
 
 export const client = createClient({
   projectId,
   dataset,
-  apiVersion,
-  useCdn,
-  token: process.env.SANITY_API_TOKEN,
-})
+  apiVersion: "2024-01-01",
+  useCdn: true,
+});
